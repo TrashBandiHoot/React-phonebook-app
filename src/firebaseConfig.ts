68073e7bd90b6ef2670924e3a64e4a3897a1fbc7
TrashBandiHoot <112197685+TrashBandiHoot@users.noreply.github.com>
2022-12-19 17:13:51 -0600

    first commit

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyA-MUuDjWa2es-PapqirTLMqIPQlaVKt7Q",
+  authDomain: "react-phonebook-dd.firebaseapp.com",
+  projectId: "react-phonebook-dd",
+  storageBucket: "react-phonebook-dd.appspot.com",
+  messagingSenderId: "295353334718",
+  appId: "1:295353334718:web:e361436043c067268497ef",
+  measurementId: "G-Q4VFV8FM4K"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { FirebaseAppProvider, AuthCheck } from 'reactfire';
+import { Home, Contact, About, Phonebook, SignIn} from './components';
+import './style.css';
+import { firebaseConfig } from './firebaseConfig';
+import 'firebase/auth';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+let myTitle = "NANANANANANANA"
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <FirebaseAppProvider firebasConfig={firebaseConfig} suspense={true}>
+    <Provider store={store}>
+    <Router>
+      <Switch>
+
+      <Route exact path='/'>
+        <Home title={myTitle} /> 
+      </Route>
+      <Route path='/phonebook'>
+        <Phonebook></Phonebook>
+      </Route>
+      <Route path='/contact'>
+        <Contact></Contact>
+      </Route>
+      <Route path='/about'>
+        <About></About>
+      </Route>
+      <Route path='/signin'>
+        <SignIn></SignIn>
+      </Route>
+
+      </Switch>
+    </Router>
+    </Provider>
+    </FirebaseAppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
